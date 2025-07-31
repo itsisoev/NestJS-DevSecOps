9858530af718bb9b628cc103a668cc1f3692b0b5
Daler <isoev9733@gmail.com>
2025-07-31 10:12:53 +0500

    добавил user profile

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { Request } from 'express';
+import { User } from './entities/user.entity';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get('profile')
+  @UseGuards(JwtAuthGuard)
+  getProfile(@Req() req: Request) {
+    const user = req.user as User;
+    return {
+      id: user.id,
+      username: user.username,
+      emails: user.emails,
+      photos: user.photos,
+    };
+  }
 }
